9e83bf6a605a351553664b1f17956ac35ad8b72c
jnarowski <jnarowski@gmail.com>
2017-12-02 12:46:38 -0700

    added bootstrap as a dependencies

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import BootstrapVue from 'bootstrap-vue'
 import router from './router'
-import store from './store';
+import store from './store'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
